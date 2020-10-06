9d481a54849a05dd6a159a2c2bb85774d69d301b
João Pedro <joao.costa@fdte.io>
2020-10-06 12:15:58 -0300

    Adicionando o logger do koa

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const json = require("koa-json");
 const KoaRouter = require("koa-router");
 const bodyParser = require("koa-bodyparser");
 const KoaCors = require("koa2-cors");
+const KoaLogger = require("koa-logger");
 
 const app = new Koa();
 const router = new KoaRouter();
@@ -12,6 +13,8 @@ app.use(json());
 app.use(bodyParser());
 
 app.use(KoaCors());
+
+app.use(KoaLogger());
 
 app.use(router.routes()).use(router.allowedMethods());
 
