6e1a71c978cdc12dc01aa00bb1c7d689fe0e48a3
João Pedro <joao.costa@fdte.io>
2020-10-06 10:53:14 -0300

    Removendo folder de views

--- routes/deltas.routes.js
+++ routes/deltas.routes.js
@@ -1,7 +1,7 @@
+const deltasController = require('../controllers/delta.controller');
+
 module.exports = (router) => {
     
-    const deltasController = require('../controllers/delta.controller');
-
     router.get('/deltas', deltasController.findAll);
 
 }
